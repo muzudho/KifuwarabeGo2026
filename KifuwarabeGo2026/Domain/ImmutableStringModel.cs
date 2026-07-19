9483fe7f2152ba2f963c92908ff815e7dd7929f4
muzudho <muzudho1@gmail.com>
2026-07-19 10:26:13 +0900

    WorkingDirectoryModel

---
+++ KifuwarabeGo2026/Domain/ImmutableStringModel.cs
@@ -0,0 +1,38 @@
+﻿namespace KifuwarabeGo2026.Domain;
+
+/// <summary>
+/// 不変文字列モデル
+/// </summary>
+internal abstract class ImmutableStringModel
+{
+    // ========================================
+    // 生成
+    // ========================================
+
+
+    /// <summary>
+    /// サブクラスでファクトリーメソッドを作って呼出してください。
+    /// </summary>
+    /// <param name="value"></param>
+    protected internal ImmutableStringModel(string value)
+    {
+        Value = value;
+    }
+
+
+    // ========================================
+    // 窓口データメンバー
+    // ========================================
+
+
+    internal bool IsEmpty => string.IsNullOrEmpty(Value);
+
+    internal string Value { get; init; }
+
+
+    // ========================================
+    // 窓口メソッド
+    // ========================================
+
+
+}
---
+++ KifuwarabeGo2026/Domain/WorkingDirectoryModel.cs
@@ -0,0 +1,44 @@
+﻿namespace KifuwarabeGo2026.Domain;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class WorkingDirectoryModel
+    : ImmutableStringModel
+{
+    // ========================================
+    // 生成
+    // ========================================
+
+    #region ［生成　＞　固定インスタンス］
+
+    internal static WorkingDirectoryModel Empty { get; } = new (string.Empty);
+
+    #endregion
+
+    #region ［生成　＞　ファクトリーメソッド］
+
+    internal static WorkingDirectoryModel FromString(string value)
+    {
+        if (string.IsNullOrEmpty(value)) return Empty;
+        return new WorkingDirectoryModel(value);
+    }
+
+    #endregion
+
+    #region ［生成　＞　コンストラクター］
+
+    /// <summary>
+    /// デフォルトコンストラクター
+    /// </summary>
+    /// <param name="value"></param>
+    internal WorkingDirectoryModel(string value)
+        : base(value)
+    {
+    }
+
+    #endregion
+}
