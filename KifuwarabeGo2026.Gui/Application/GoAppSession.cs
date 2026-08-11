70e22260452f1d4c31e35b8efc946eae9355fae8
高橋 智史 <muzudho1@gmail.com>
2026-08-11 16:35:15 +0900

    wip

--- KifuwarabeGo2026.Gui/Application/GoAppSession.cs
+++ KifuwarabeGo2026.Gui/Application/GoAppSession.cs
@@ -17,10 +17,8 @@ public sealed partial class GoAppSession
 {
     private GoBoard _board;
     private readonly HashSet<ulong> _positionHashes = new();
-    private readonly List<TournamentRules> _tournamentRules = new();
     private readonly List<GtpEngineProfile> _gtpEngineProfiles = new();
     private readonly List<GtpEngineAppCompatibility> _gtpEngineAppCompatibilities = new();
-    private TournamentRules _currentTournamentRules = new();
 
     public GoAppSession()
     {
@@ -32,58 +30,6 @@ public sealed partial class GoAppSession
     }
 
     public int BoardSize { get; private set; } = 19;
-
-    public IReadOnlyList<TournamentRules> TournamentRulesList => _tournamentRules;
-
-    public CatalogOrderEditor<TournamentRules> TournamentRulesOrderEditor { get; } = new();
-
-    public int SelectedTournamentRulesIndex { get; private set; }
-
-    public bool IsTournamentRulesSelectionDialogOpen { get; private set; }
-
-    public int TournamentRulesDialogSelectionIndex { get; private set; }
-
-    public bool IsTournamentRulesAddPanelOpen { get; private set; }
-
-    public bool IsTournamentRulesEditPanelMode { get; private set; }
-
-    public bool IsTournamentRulesDeleteConfirmationOpen { get; private set; }
-
-    public string TournamentRulesDeleteConfirmationFileName { get; private set; } = "";
-
-    public int TournamentRulesSelectionPageIndex { get; private set; }
-
-    public string TournamentRulesSaveMessage { get; private set; } = "";
-
-    public string TournamentRulesDisplayNameDraft { get; private set; } = "";
-
-    public bool IsTournamentRulesDisplayNameEditing { get; private set; }
-
-    public int TournamentRulesDisplayNameCaretIndex { get; private set; }
-    public int TournamentRulesDisplayNameSelectionStart { get; private set; }
-    public int TournamentRulesDisplayNameSelectionLength { get; private set; }
-
-    public string TournamentRulesDisplayNameWarning { get; private set; } = "";
-
-    public TournamentRulesNumericField? ActiveTournamentRulesNumericField { get; private set; }
-
-    public string TournamentRulesNumericDraft { get; private set; } = "";
-
-    public int TournamentRulesNumericCaretIndex { get; private set; }
-    public int TournamentRulesNumericSelectionStart { get; private set; }
-    public int TournamentRulesNumericSelectionLength { get; private set; }
-
-    public string TournamentDisplayName => _currentTournamentRules.DisplayName;
-
-    public GoRuleKind RuleKind => _currentTournamentRules.Rule;
-
-    public decimal Komi => _currentTournamentRules.Komi;
-
-    public TimeSpan MainTime => _currentTournamentRules.MainTime;
-
-    public int MoveLimit => _currentTournamentRules.MoveLimit;
-
-    public TournamentRules CurrentTournamentRules => _currentTournamentRules.Clone();
 
     public IReadOnlyList<GtpEngineProfile> GtpEngineProfiles => _gtpEngineProfiles;
 
